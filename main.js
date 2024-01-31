a45aeed3daa1ce2af9524228aa555563636de362
Farkas <farkas.dano@gmail.com>
2024-01-31 13:39:06 +0100

    Legkisább szám vizsgálatához gomb hozzáadása, js oldalon is lekezelve, logika megírva és visszaadja a legkisebb számot a listából.
    (4. Feladat)

--- main.js
+++ main.js
@@ -1,34 +1,39 @@
 function beolvasSzam() {
-
-    //Grafikus felületű alkalmazás esetén a bevitel alá megfelelő elembe (bekezdés, label, stb.) írd ki a számokat hozzáadás után
-    //Hozzáadás után írd ki a lista elemeit vesszővel elválasztva
     var szamErtek = document.getElementById('szam').value;
 
     if (!isNaN(szamErtek)) {
-      // Lista elem létrehozása és hozzáadása a listához
       var listaElem = document.createElement('li');
       listaElem.textContent = szamErtek;
 
       var lista = document.getElementById('szamokLista');
 
-      // Ellenőrzés, hogy már van-e lista a HTML-ben
       if (!document.getElementById('szamLista')) {
-        // Ha nincs, akkor létrehoz egy listát
         var szamLista = document.createElement('ul');
         szamLista.setAttribute('id', 'szamLista');
         document.body.appendChild(szamLista);
       }
 
-      // Hozzáadás az előre létrehozott listához
       document.getElementById('szamLista').appendChild(listaElem);
-      
-      // Üresre állítja a bemeneti mezőt
       document.getElementById('szam').value = '';
     } else {
       alert('Érvénytelen szám!');
     }
-
-    console.log(szamErtek);
   }
-    
-  
+
+  function megtalalLegkisebb() {
+    var listaElemek = document.getElementById('szamLista').getElementsByTagName('li');
+    var legkisebb = Infinity;
+
+    for (var i = 0; i < listaElemek.length; i++) {
+      var szam = parseFloat(listaElemek[i].textContent);
+      if (!isNaN(szam) && szam < legkisebb) {
+        legkisebb = szam;
+      }
+    }
+
+    if (legkisebb !== Infinity) {
+      document.getElementById('legkisebbSzam').textContent = 'Legkisebb szám: ' + legkisebb;
+    } else {
+      document.getElementById('legkisebbSzam').textContent = 'Nincs megadott szám a listában.';
+    }
+  }
